7932d79daf3fecd56026ea4b873960b015ab247a
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-07-06 12:59:37 +0200

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,38 +1,40 @@
 #include "holberton.h"
+#define NULL 0
 
 /**
- * _strstr - locates a substring
- * @haystack: string to search through
- * @needle: string to match
- * Return: pointer to initial segment of haystack
+ * _strstr - locate and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ * Return: pointer to index of string at first occurence of whole substring
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, start;
-	int nlen = 0;
+	int i = 0, j, x;
 
-	i = 0;
-	j = 0;
-	start = 0;
+	if (needle[0] == '\0')
+		return (haystack);
 
-	while (needle[nlen] != '\0')
+	while (haystack[i] != '\0') /* iterate through haystack */
 	{
-		nlen++;
-	}
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; j < nlen && haystack[i] == needle[j]; j++, i++)
+		/* if a byte matches first char of needle */
+		/* interate through needle until match ends */
+		if (haystack[i] == needle[0])
 		{
-			if (j == 0)
+			x = i, j = 0;
+			while (needle[j] != '\0')
 			{
-				start = i;
-			}
-			if (j == nlen - 1)
+				if (haystack[x] == needle[j])
+					x++, j++;
+				else
+					break;
+			} /* if matched throughout, return haystack */
+			if (needle[j] == '\0')
 			{
-				return (haystack + start);
+				return (haystack + i);
 			}
 		}
+		i++;
 	}
-	return (0);
+	return (NULL); /* No match */
 }
