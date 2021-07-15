6cd645d0072c27fa67adc4c305ad7c79fc4c2005
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-07-15 19:43:33 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,68 +1,56 @@
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
- * _strlen - gets string length
+ * _strlen - calculate and return string length
  * @string: string
- * Return: length (int)
+ * Return: string length
  */
 
 int _strlen(char *string)
 {
-	int length;
+	int i;
 
-	length = 0;
-	while (string[length] != '\0')
-	{
-		length++;
-	}
-	return (length);
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
 }
 
 /**
- * string_nconcat - concatenates two strings
+ * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
  * @s1: string 1
  * @s2: string 2
- * @n: integer that determines whether entire s2 string will be used
- * Return: pointer to allocated memory corresponding with the string
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int length1, length2, lengthc, i, j;
-	char *str;
+	char *ptr;
+	int num, len, i, j;
 
-	if (s1 == NULL)
+	num = n;
+
+	if (s1 == NULL) /* account for NULL strings */
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	length1 = _strlen(s1);
-	length2 = _strlen(s2);
+	if (num < 0) /* account for negative n bytes */
+		return (NULL);
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
 
-	if (n >= length2)
-		lengthc = length1 + length2 + 1;
-	else
-	{
-		lengthc = length1 + n + 1;
-		length2 = n;
-	}
-	str = malloc(lengthc);
-	if (str == NULL)
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	if (ptr == NULL)
 		return (NULL);
 
-	i = 0;
-	while (i < length1)
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (j < length2)
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	return (str);
-}
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
 
+	return (ptr);
+}
