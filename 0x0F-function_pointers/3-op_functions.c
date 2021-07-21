b44868f33bf6712c81b7b8f144e646373938d87c
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-07-21 22:04:48 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,73 +1,54 @@
-#include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * op_add - sum of two numbers
- * @a: first integer
- * @b: second integer
- * Return: sum (integer)
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - difference between two numbers
- * @a: first integer
- * @b: second integer
- * Return: difference (integer)
+ * op_sub - subtract
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - product of two numbers
- * @a: first integer
- * @b: second integer
- * Return: product (integer)
+ * op_mul - multiply
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - quotient of two numbers
- * @a: first integer (dividend)
- * @b: second integer (divisor)
- * Return: quotient (integer)
+ * op_div - divide
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
-
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
 /**
- * op_mod - modulo (remainder)
- * @a: first integer (dividend)
- * @b: second integer (divisor)
- * Return: remainder of division (integer)
+ * op_mod - modulus for remainder
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
-
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
