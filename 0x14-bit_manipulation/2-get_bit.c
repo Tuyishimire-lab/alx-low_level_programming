3fe29f1c86dd489ab63399a985d5fcaedcb1fa1a
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-08-05 18:42:36 +0200

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,23 @@
 #include "holberton.h"
 
 /**
- * get_bit - returns the value of a bit at a given index
- * @n: integer
- * @index: index
- * Return: integer
+ * get_bit - get bit at index
+ * @n: number
+ * @index: index within binary number
+ * Return: bit 0 or 1, or -1 if error
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	int bit;
+	unsigned int max_bits;
+
+	/* validate index is not out of range */
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
 		return (-1);
-	return ((n >> index) & 1);
+
+	/* shift number index places right to find bit */
+	bit = ((n >> index) & 1);
+
+	return (bit);
 }
