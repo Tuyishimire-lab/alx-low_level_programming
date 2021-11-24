dee23b41800fb815d5bcdf7dd0b6eff4c04c033e
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-11-24 16:28:18 +0200

    Testing

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,12 +11,11 @@ def island_perimeter(grid):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
                 if row == 0 or grid[row - 1][col] == 0:
-                    p += 1
+                    p += 1  # top
                 if row == (len(grid) - 1) or grid[row + 1][col] == 0:
-                    p += 1
+                    p += 1  # bottom
                 if col == 0 or grid[row][col - 1] == 0:
-                    p += 1
+                    p += 1  # left
                 if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
-                    p += 1
+                    p += 1  # right
     return p
-
